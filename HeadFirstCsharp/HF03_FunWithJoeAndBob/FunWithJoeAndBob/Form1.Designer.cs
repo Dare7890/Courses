4d55636c0a21e939cad49aff65b22a2dd849f9a6
Dare7890 <vladfetisov2011@yandex.ru>
2020-09-19 00:39:41 +0300

    Third day

---
+++ HeadFirstCsharp/HF03_FunWithJoeAndBob/FunWithJoeAndBob/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace FunWithJoeAndBob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.joesCashLabel = new System.Windows.Forms.Label();
+            this.bobsCashLabel = new System.Windows.Forms.Label();
+            this.bankCashLabel = new System.Windows.Forms.Label();
+            this.btnGive = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.joeGivesToBob = new System.Windows.Forms.Button();
+            this.bobGivesToJoe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // joesCashLabel
+            // 
+            this.joesCashLabel.AutoSize = true;
+            this.joesCashLabel.Location = new System.Drawing.Point(82, 38);
+            this.joesCashLabel.Name = "joesCashLabel";
+            this.joesCashLabel.Size = new System.Drawing.Size(86, 17);
+            this.joesCashLabel.TabIndex = 0;
+            this.joesCashLabel.Text = "Joe has $50";
+            // 
+            // bobsCashLabel
+            // 
+            this.bobsCashLabel.AutoSize = true;
+            this.bobsCashLabel.Location = new System.Drawing.Point(82, 93);
+            this.bobsCashLabel.Name = "bobsCashLabel";
+            this.bobsCashLabel.Size = new System.Drawing.Size(96, 17);
+            this.bobsCashLabel.TabIndex = 1;
+            this.bobsCashLabel.Text = "Bob has $100";
+            // 
+            // bankCashLabel
+            // 
+            this.bankCashLabel.AutoSize = true;
+            this.bankCashLabel.Location = new System.Drawing.Point(82, 157);
+            this.bankCashLabel.Name = "bankCashLabel";
+            this.bankCashLabel.Size = new System.Drawing.Size(131, 17);
+            this.bankCashLabel.TabIndex = 2;
+            this.bankCashLabel.Text = "The bank has $100";
+            // 
+            // btnGive
+            // 
+            this.btnGive.Location = new System.Drawing.Point(85, 198);
+            this.btnGive.Name = "btnGive";
+            this.btnGive.Size = new System.Drawing.Size(167, 65);
+            this.btnGive.TabIndex = 3;
+            this.btnGive.Text = "Give $10 to Joe";
+            this.btnGive.UseVisualStyleBackColor = true;
+            this.btnGive.Click += new System.EventHandler(this.btnGive_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(325, 198);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(167, 65);
+            this.btnReceive.TabIndex = 4;
+            this.btnReceive.Text = "Receive $5 from Bob";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // joeGivesToBob
+            // 
+            this.joeGivesToBob.Location = new System.Drawing.Point(85, 277);
+            this.joeGivesToBob.Name = "joeGivesToBob";
+            this.joeGivesToBob.Size = new System.Drawing.Size(167, 65);
+            this.joeGivesToBob.TabIndex = 5;
+            this.joeGivesToBob.Text = "Joe gives $10 to Bob";
+            this.joeGivesToBob.UseVisualStyleBackColor = true;
+            this.joeGivesToBob.Click += new System.EventHandler(this.joeGivesToBob_Click);
+            // 
+            // bobGivesToJoe
+            // 
+            this.bobGivesToJoe.Location = new System.Drawing.Point(325, 277);
+            this.bobGivesToJoe.Name = "bobGivesToJoe";
+            this.bobGivesToJoe.Size = new System.Drawing.Size(167, 65);
+            this.bobGivesToJoe.TabIndex = 6;
+            this.bobGivesToJoe.Text = "Bob gives $5 to Joe";
+            this.bobGivesToJoe.UseVisualStyleBackColor = true;
+            this.bobGivesToJoe.Click += new System.EventHandler(this.bobGivesToJoe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 354);
+            this.Controls.Add(this.bobGivesToJoe);
+            this.Controls.Add(this.joeGivesToBob);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnGive);
+            this.Controls.Add(this.bankCashLabel);
+            this.Controls.Add(this.bobsCashLabel);
+            this.Controls.Add(this.joesCashLabel);
+            this.Name = "Form1";
+            this.Text = "Fun with Joe and Bob";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label joesCashLabel;
+        private System.Windows.Forms.Label bobsCashLabel;
+        private System.Windows.Forms.Label bankCashLabel;
+        private System.Windows.Forms.Button btnGive;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button joeGivesToBob;
+        private System.Windows.Forms.Button bobGivesToJoe;
+    }
+}
+
---
+++ HeadFirstCsharp/HF03_TalkerTester/HF03_TalkerTester/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace HF03_TalkerTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSay = new System.Windows.Forms.Label();
+            this.lblTimes = new System.Windows.Forms.Label();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.nudTimes = new System.Windows.Forms.NumericUpDown();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblSay
+            // 
+            this.lblSay.AutoSize = true;
+            this.lblSay.Location = new System.Drawing.Point(43, 47);
+            this.lblSay.Name = "lblSay";
+            this.lblSay.Size = new System.Drawing.Size(62, 17);
+            this.lblSay.TabIndex = 0;
+            this.lblSay.Text = "Say this:";
+            // 
+            // lblTimes
+            // 
+            this.lblTimes.AutoSize = true;
+            this.lblTimes.Location = new System.Drawing.Point(43, 100);
+            this.lblTimes.Name = "lblTimes";
+            this.lblTimes.Size = new System.Drawing.Size(73, 17);
+            this.lblTimes.TabIndex = 1;
+            this.lblTimes.Text = "# of times:";
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(178, 44);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(238, 22);
+            this.tbText.TabIndex = 2;
+            this.tbText.Text = "Hello!";
+            // 
+            // nudTimes
+            // 
+            this.nudTimes.Location = new System.Drawing.Point(178, 98);
+            this.nudTimes.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudTimes.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudTimes.Name = "nudTimes";
+            this.nudTimes.Size = new System.Drawing.Size(90, 22);
+            this.nudTimes.TabIndex = 3;
+            this.nudTimes.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(178, 144);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(162, 23);
+            this.btnSpeak.TabIndex = 4;
+            this.btnSpeak.Text = "Speak to me!";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 188);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.nudTimes);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.lblTimes);
+            this.Controls.Add(this.lblSay);
+            this.Name = "Form1";
+            this.Text = "Talker Tester";
+            ((System.ComponentModel.ISupportInitialize)(this.nudTimes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSay;
+        private System.Windows.Forms.Label lblTimes;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.NumericUpDown nudTimes;
+        private System.Windows.Forms.Button btnSpeak;
+    }
+}
+
